294b6e7a82162ed3f3836d7ece105a4ca9bbaac9
GG-Windy <bm.hcmc.windy@gmail.com>
2025-07-26 21:17:00 +0700

    [Art] Move with joystick

---
+++ Shooter3D/Assets/Shooter3D/Scripts/PlayerMovementJoystick.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class PlayerMovementJoystick : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    public VariableJoystick joystick; // gắn từ inspector
+
+    private Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+
+    }
+
+    void FixedUpdate()
+    {
+        Vector3 move = new Vector3(joystick.Horizontal, 0, joystick.Vertical);
+        rb.MovePosition(rb.position + move * moveSpeed * Time.fixedDeltaTime);
+
+        // Quay nhân vật theo hướng di chuyển nếu cần
+        if (move != Vector3.zero)
+        {
+            Quaternion toRotation = Quaternion.LookRotation(move, Vector3.up);
+            rb.rotation = Quaternion.RotateTowards(rb.rotation, toRotation, 500 * Time.fixedDeltaTime);
+        }
+    }
+}
