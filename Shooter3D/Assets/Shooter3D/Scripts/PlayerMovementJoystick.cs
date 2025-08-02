6ffc09c23b29679977ad989df58824d117476c0c
GG-Windy <bm.hcmc.windy@gmail.com>
2025-08-02 21:49:17 +0700

    [Art] Animation player move

--- Shooter3D/Assets/Shooter3D/Scripts/PlayerMovementJoystick.cs
+++ Shooter3D/Assets/Shooter3D/Scripts/PlayerMovementJoystick.cs
@@ -8,16 +8,19 @@ public class PlayerMovementJoystick : MonoBehaviour
     public VariableJoystick joystick; // gắn từ inspector
 
     private Rigidbody rb;
+    private Animator animator;
 
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        animator = GetComponent<Animator>(); // Get Animator on the same GameObject
 
     }
 
     void FixedUpdate()
     {
         Vector3 move = new Vector3(joystick.Horizontal, 0, joystick.Vertical);
+        // Move player
         rb.MovePosition(rb.position + move * moveSpeed * Time.fixedDeltaTime);
 
         // Quay nhân vật theo hướng di chuyển nếu cần
@@ -26,5 +29,8 @@ public class PlayerMovementJoystick : MonoBehaviour
             Quaternion toRotation = Quaternion.LookRotation(move, Vector3.up);
             rb.rotation = Quaternion.RotateTowards(rb.rotation, toRotation, 500 * Time.fixedDeltaTime);
         }
+
+        // Update Animator parameter based on movement magnitude
+        animator.SetFloat("Speed", move.magnitude); // you can also smooth this with damping
     }
 }
